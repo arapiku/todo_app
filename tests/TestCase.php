cdc39b75eb70953545f23e596ecc1411e9dfcbd6
arapiku <arapiku420@gmail.com>
2017-08-23 14:30:16 +0900

    browser kit testingでテストできる仕様に変更

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+// use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    // 環境により要変更
+    public $baseUrl = 'http://192.168.35.10';
 }
